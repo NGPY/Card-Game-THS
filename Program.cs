47c21f621b33909d21fe8ec737e9a5327e3246ac
B Meidus <benediktas.meidus@gmail.com>
2023-02-07 12:15:58 +0000

    Added comments

--- Program.cs
+++ Program.cs
@@ -2,36 +2,47 @@
 {
     class Player
     {
+        // Properties
         private string name;
         private int score;
-
-        public Player(string givenName) // Constructor
+        // Constructors
+        public Player(string givenName)
         {
             this.name = givenName;
             this.score = 0;
         }
+        // Getters and Setters
+        public string Name { get { return name; } }
+        public int Score { get { return score; } set { score = value; } }
+
 
     }
     class PlayingCard
     {
+        // Properties
         private string suite;
         private int value;
         private string rank;
-
+        // Constructor
         public PlayingCard(string suite, string rank, int value)
         {
             this.suite = suite;
             this.value = value;
             this.rank = rank;
         }
+        // Getters and Setters
+        public string Suite { get { return suite; } }
+        public int Value { get { return value; } }
+        public string Rank { get { return rank; } }
     }
     class Deck
     {
+        // Properties
         private PlayingCard[] cards = new PlayingCard[52];
         private string[] suits = { "clubs", "diamonds", "hearts", "spades" };
         private string[] ranks = { "ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king" };
         private int[] values = { 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10 };
-
+        // Constructor
         public Deck()
         {
             int cardNumber = 0;
@@ -44,16 +55,15 @@
                 }
             }
         }
+        // Getters and setters
+        public PlayingCard[] Cards { get { return cards; } }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Name");
-            string PlayerName = Console.ReadLine();
-            Player gamePlayer = new Player(PlayerName);
-            string PlayerName1 = Console.ReadLine();
-            Player DooDoo = new Player(PlayerName1);
+            PlayingCard queen = new PlayingCard("hearts", "queen", 10);
+            Console.WriteLine(queen.Rank);
         }
     }
 }
